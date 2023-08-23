a17ce3cd819cbe1e7c28bd9096ff7485985e037d
Andy <andys920605@gmail.com>
2023-08-23 15:30:05 +0800

    add time range filter to get top five transations

--- backend/router/api.go
+++ backend/router/api.go
@@ -7,6 +7,9 @@ import (
 	"log"
 	"net/http"
 	m "sleuth/models/router"
+	"sort"
+	"strconv"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
@@ -21,13 +24,16 @@ func (router *Router) version(c *gin.Context) {
 
 func (router *Router) getAssetTransfers(c *gin.Context) {
 	address := c.Query("address")
+	tr := c.DefaultQuery("time_range", "60")
+	timeRange, err := strconv.Atoi(tr)
+
 	if address == "" {
 		c.JSON(http.StatusBadRequest, gin.H{
 			"message": "Address parameter is required",
 		})
 		return
 	}
-	url := "https://api.etherscan.io/api?module=account&action=txlist&address=" + address + "&startblock=0&endblock=latest&page=1&offset=10&sort=desc&apikey=" + router.infra.Config.Etherscan.Token
+	url := "https://api.etherscan.io/api?module=account&action=txlist&address=" + address + "&startblock=0&endblock=latest&page=1&offset=1000&sort=desc&apikey=" + router.infra.Config.Etherscan.Token
 
 	resp, err := http.Get(url)
 	if err != nil {
@@ -51,10 +57,14 @@ func (router *Router) getAssetTransfers(c *gin.Context) {
 
 	if rsp.Message != "OK" {
 		c.JSON(http.StatusBadRequest, gin.H{
-			"message": fmt.Sprintf("error: %s", rsp.Message),
+			"message": rsp.Message,
 		})
 		return
 	}
+
+	transactionsWithinRange := filterTransactionsWithinTimeRange(rsp, timeRange)
+	TopFiveTransactionsByAmount := findTopFiveTransactionsByAmount(transactionsWithinRange)
+	rsp.Result = TopFiveTransactionsByAmount
 
 	c.JSON(http.StatusOK, rsp)
 }
@@ -97,4 +107,46 @@ func (router *Router) getBalance(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusOK, rsp)
+}
+
+func filterTransactionsWithinTimeRange(transactions m.TransactionResponse, timeRange int) []m.Transaction {
+
+	var target []m.Transaction
+
+	now := time.Now().Unix()
+
+	for i, v := range transactions.Result {
+		timeStamp, err := strconv.ParseInt(v.TimeStamp, 10, 64)
+		if err == nil {
+			t := time.Unix(timeStamp, 0)
+
+			secondsDiff := now - t.Unix()
+
+			if secondsDiff <= int64(timeRange) {
+				target = append(target, transactions.Result[i])
+			} else {
+				break
+			}
+		}
+	}
+
+	return target
+}
+
+func findTopFiveTransactionsByAmount(transactions []m.Transaction) []m.Transaction {
+	sort.Slice(transactions, func(i, j int) bool {
+		amountI, errI := strconv.ParseFloat(transactions[i].Value, 64)
+		amountJ, errJ := strconv.ParseFloat(transactions[j].Value, 64)
+		if errI != nil || errJ != nil {
+			return false
+		}
+		return amountI > amountJ
+	})
+
+	var target []m.Transaction
+	for i := 0; i < len(transactions) && i < 5; i++ {
+		target = append(target, transactions[i])
+	}
+
+	return target
 }
